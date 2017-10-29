c69977d6ad5f354cefcf713cb8a6a6c2176bdfd4
Narendra Singh <narendra0014@gmail.com>
2017-10-29 02:36:45 +0200

    First commit. Start of our project journey.

---
+++ src/runGame.java
@@ -0,0 +1,10 @@
+/* Forest Runner project by Narendra Singh and Mazin Housin */
+
+public class runGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
